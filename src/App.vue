d62e7726ef2287780b4616112a1557caa95430f9
Mateusz <ogonekmateusz69@gmail.com>
2024-04-13 20:01:15 +0200

    add styles to header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
 <template>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+   <HeaderView/>
   <router-view/>
 </template>
 
+<script setup lang="ts">
+  import HeaderView from "./components/HeaderView.vue"
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
