63ed038b5e4af8e919c48e9bcc4022504812e233
Mateusz <ogonekmateusz69@gmail.com>
2024-04-14 13:45:04 +0200

    style block in sectionService

--- src/components/SectionServices.vue
+++ src/components/SectionServices.vue
@@ -1,11 +1,13 @@
 <template>
     <section>
       <h2>7+ years experience working</h2>
-      <div class="box">
-        <img :src="require('@/assets/icon-1.png')" alt="img">
-        <h3>{{ title }}</h3>
-        <p>{{ description }}</p>
-      </div>
+      <section class="cardServices">
+        <div class="box">
+            <img :src="require('@/assets/icon-1.png')" alt="img">
+            <h3>{{ title }}</h3>
+            <p>{{ description }}</p>
+        </div>
+      </section>
     </section>
   </template>
   
@@ -18,6 +20,35 @@
   </script>
   
   <style scoped>
-  /* Dodaj style jeśli potrzebujesz */
+
+    section {
+        padding: 80px 100px;
+    }
+  
+    h2 {
+        color: var(--white);
+        font-weight: bold;
+        font-size: 46px;
+        line-height: 56px;
+    }
+
+    .box {
+        margin: 46px;
+        background-color:var(--lightDark) ;
+        padding: 68px 25px;
+    }
+
+    .box h3 {
+        line-height: 32px;
+        font-size: 26px;
+        font-weight: bold;
+        color: var(--white);
+        margin: 18px 0;
+    }
+    .box p {
+        font-size: 16px;
+        line-height: auto;
+        color: var(--gray);
+    }
   </style>
   
