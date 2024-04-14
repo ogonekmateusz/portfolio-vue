6740be664bf74fc0a15c0606cf99e1794100b1e1
Mateusz <ogonekmateusz69@gmail.com>
2024-04-14 21:32:08 +0200

    Add HTML and CSS to the "works" section and create a component called "SectionWork".

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,7 +19,20 @@
         :description="service.description" 
         />
     </div>
-    </section>
+  </section>
+  <section class="works">
+    <div class="top-block">
+      <hr class="line"> 
+      <span>Recent Work</span>
+    </div>
+    <div class="works-header">
+      <h2>Some of my favorite <br> projects.</h2>
+      <div class="services-header-right-block">
+        <button>View All Project</button>
+      </div>
+    </div>
+    <!-- wyswieltenie komponentu Section work -->
+  </section>
 </template>
 
 <script lang="ts" setup>
@@ -49,29 +62,30 @@ const services: service[] = [
     description: "I help design strong and captivating brand strategies. Through thorough research and market analysis."
   }
 ];
-
-
-
 </script>
+
 <style>
 .wrapper{
   width: 100%;
 }
+
 .services{
   .cardServices{
     width: 100%;
     justify-content: center;
     display: flex;
   }
+
   .services-header{
     width: 100%;
     justify-content: space-around;
     display: flex;
     align-items: center;
+
     .services-header-right-block{
       .right-block-wrapper{
         .line{
-          background-color:var(--primary) ;
+          background-color: var(--primary);
           width: 50px;
           border: 1.9px;
           height: 2px;
@@ -92,5 +106,57 @@ h2{
   font-weight: bold;
   font-size: 46px;
   line-height: 56px;
+}
+
+.works {
+  margin-top: 50px;
+  background: var(--lightDark);
+  padding: 80px 100px;
+
+  .top-block {
+    width: 100%;
+    .line {
+      background-color: var(--primary);
+      width: 50px;
+      border: 1.9px;
+      height: 2px;
+      float: left;
+      margin-right: 10px;
+    }
+    span {
+      color: var(--primary);
+      font-weight: bold;
+    }
+  }
+
+  .works-header {
+    width: 100%;
+    justify-content: space-between;
+    display: flex;
+    align-items: center;
+
+    h2 {
+      margin: 10px 0;
+    }
+
+    .services-header-right-block{
+      button {
+        font-size: 14px;
+        background-color: transparent;
+        font-weight: bold;
+        padding: 10px 16px;
+        border: 1px solid var(--primary);
+        color: white;
+        cursor: pointer;
+        transition: color 0.3s ease-in-out;
+      }
+
+      button:hover {
+        background-color: var(--primary);
+        border-color: var(--lightDark);
+        color: var(--lightDark);
+      }
+    }
+  }
 }
 </style>
