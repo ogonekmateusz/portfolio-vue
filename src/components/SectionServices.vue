394b53fcda41b8ee6abecb5ba2d55d0ef0fa7485
Mateusz <ogonekmateusz69@gmail.com>
2024-04-14 13:20:07 +0200

    repair props

--- src/components/SectionServices.vue
+++ src/components/SectionServices.vue
@@ -1,25 +1,23 @@
 <template>
     <section>
-        <h2>7+ years experience working</h2>
-        <div class="box">
-            <img :src="imgSrc" alt="img">
-            <h3>{{ title }}</h3>
-            <p>
-                {{ description }}
-            </p>
-        </div>
+      <h2>7+ years experience working</h2>
+      <div class="box">
+        <img :src="require('@/assets/icon-1.png')" alt="img">
+        <h3>{{ title }}</h3>
+        <p>{{ description }}</p>
+      </div>
     </section>
   </template>
   
   <script lang="ts" setup>
   defineProps<{
-    title:string,
-    imgSrc:string,
-    description:string
+    title: string,
+    imgSrc: string,
+    description: string
   }>()
   </script>
   
   <style scoped>
-  
+  /* Dodaj style jeśli potrzebujesz */
   </style>
   
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <template>
   <SectionHero/>
-  <SectionServices img_src="../assets/icon-1.png" title="Product Designer" description="I offer innovative and attenion-grabbing product design services. From inital ideation to implementation" />
-  <img src="../assets/icon-1.png" alt="cosik">
+  <SectionServices imgSrc=".../assets/icon-1.png" title="Product Designer" description="I offer innovative and attention-grabbing product design services. From initial ideation to implementation" />
 </template>
+
 <script lang="ts" setup>
 import SectionHero from "../components/SectionHero.vue";
 import SectionServices from "../components/SectionServices.vue";
-
 </script>
-  
