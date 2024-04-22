4888c282414132b38d3b9de5bb4ce3f63a991fea
Mateusz <ogonekmateusz69@gmail.com>
2024-04-22 20:46:36 +0200

    Add responsive header

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark p-3" aria-label="Fourth navbar example">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Expand at md</a>
+      <a class="navbar-brand" href="#">Alifreza<span>.</span> </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -9,24 +9,24 @@
       <div class="collapse navbar-collapse" id="navbarsExample04">
         <ul class="navbar-nav ms-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <RouterLink class="nav-link" to="/">Home</RouterLink>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <RouterLink class="nav-link" to="/">Services</RouterLink>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            <RouterLink class="nav-link" to="/">Work</RouterLink>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/">About us</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link " to="/">Blog</RouterLink>
+          </li>
+          <li class="nav-item ">
+            <RouterLink class="nav-link" to="/">Contact</RouterLink>
           </li>
         </ul>
-      
       </div>
     </div>
   </nav>
@@ -39,12 +39,54 @@ import { RouterLink } from 'vue-router';
 </script>
   
 <style scoped>
-
-body {
-  padding-bottom: 20px;
+.navbar {
+  background-color: var(--dark) !important;
+  font-weight: bold;
+  width: 100%;
+  position: fixed;
+  left: 0; 
+  top: 0; 
 }
 
-.navbar {
-  margin-bottom: 20px;
+.navbar-brand {
+  font-size: 24px;
+  color: var(--white);
+  line-height: 28px;
+}
+
+.navbar-brand span {
+  color: var(--primary);
+}
+
+.nav-link {
+  position: relative;
+  color: var(--white);
+  text-decoration: none;
+  font-size: 14px;
+  line-height: auto;
+  font-weight: bold;
+  padding: 10px 15px;
+  transition: color 0.3s ease-in-out;
+  margin: 0 5px;
+}
+
+.nav-item {
+  position: relative;
+}
+
+.nav-item::after {
+  content: "";
+  position: absolute;
+  bottom: -2px;
+  left: 0;
+  width: 0%;
+  height: 2px;
+  background-color: var(--primary);
+  transform-origin: right;
+  transition: width 0.3s ease-in-out;
+}
+
+.nav-item:hover::after {
+  width: 100%;
 }
 </style>
