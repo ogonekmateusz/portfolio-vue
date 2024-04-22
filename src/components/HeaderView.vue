267d5407ff0e373c90ad315c46fd720673a4343c
Mateusz <ogonekmateusz69@gmail.com>
2024-04-22 23:00:29 +0200

    refactor css code

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -23,8 +23,8 @@
           <li class="nav-item">
             <RouterLink class="nav-link " to="/">Blog</RouterLink>
           </li>
-          <li class="nav-item ">
-            <RouterLink class="nav-link" to="/">Contact</RouterLink>
+          <li class="nav-item border ">
+            <RouterLink class="nav-link last" to="/">Contact</RouterLink>
           </li>
         </ul>
       </div>
@@ -39,8 +39,11 @@ import { RouterLink } from 'vue-router';
 </script>
   
 <style scoped>
+
+@import url(/src/assets/colors.css);
+/* Styl dla nawigacji */
 .navbar {
-  background-color: var(--dark) !important;
+  background-color: var(--dark) !important; /* Ustawienie koloru tła paska nawigacji */
   font-weight: bold;
   width: 100%;
   position: fixed;
@@ -48,32 +51,43 @@ import { RouterLink } from 'vue-router';
   top: 0; 
 }
 
+/* Styl dla marki */
 .navbar-brand {
   font-size: 24px;
-  color: var(--white);
+  color: var(--white); /* Kolor tekstu marki */
   line-height: 28px;
 }
 
 .navbar-brand span {
-  color: var(--primary);
+  color: var(--primary); /* Kolor dodatkowego punktu w marki */
 }
 
+/* Styl dla linków nawigacyjnych */
 .nav-link {
   position: relative;
-  color: var(--white);
+  color: var(--white); /* Kolor tekstu linków */
   text-decoration: none;
   font-size: 14px;
   line-height: auto;
   font-weight: bold;
-  padding: 10px 15px;
-  transition: color 0.3s ease-in-out;
-  margin: 0 5px;
+  transition: color 0.3s ease-in-out; /* Animacja zmiany koloru */
+  margin: 0 12px;
 }
 
+/* Styl dla ostatniego elementu nawigacji */
+.nav-item:last-child {
+  border-color: var(--primary) !important; /* Kolor obramowania ostatniego elementu */
+  margin-left: 20px;
+  transition: .3s ease-in-out;
+  cursor: pointer;
+}
+
+/* Styl dla pozostałych elementów nawigacji */
 .nav-item {
   position: relative;
 }
 
+/* Animacja dla elementów nawigacyjnych */
 .nav-item::after {
   content: "";
   position: absolute;
@@ -83,10 +97,79 @@ import { RouterLink } from 'vue-router';
   height: 2px;
   background-color: var(--primary);
   transform-origin: right;
-  transition: width 0.3s ease-in-out;
+  transition: width 0.35s ease-in-out;
 }
 
-.nav-item:hover::after {
+/* Animacja dla elementów nawigacyjnych po najechaniu myszką */
+.nav-item:not(:last-child):hover::after {
   width: 100%;
 }
+
+/* Styl dla ostatniego elementu nawigacji po najechaniu myszką */
+.nav-item:last-child:hover {
+  background-color: var(--primary) !important;
+  border-color: var(--dark) !important;
+}
+
+.last:hover {
+  color: black !important;
+}
+
+/* Media queries dla ekranów o szerokości do 768px */
+@media (max-width: 768px) {
+
+.nav-item {
+  padding: 5px 0;
+}
+
+.nav-link {
+  margin: 0;
+}
+
+.nav-item::after {
+  background-color: transparent;
+  left: 10px;
+}
+
+.nav-item:not(:last-child):hover::after {
+  width: 15%;
+}
+
+.nav-item:last-child {
+  margin: 0;
+  border: none !important;
+}
+
+.last:hover {
+  color: var(--white) !important; 
+}
+
+.navbar-nav > .nav-item:last-child:hover {
+  background-color: transparent !important; 
+  border-color: transparent !important; 
+  color: var(--white) !important;
+}
+
+.navbar-nav > .nav-item:last-child:hover::after {
+  width: 0; 
+}
+
+.last::after, .nav-item::after {
+  content: "";
+  position: absolute;
+  bottom: -2px;
+  left: 0;
+  width: 0%;
+  height: 2px;
+  background-color: var(--primary);
+  transform-origin: right;
+  transition: width 0.35s ease-in-out;
+}
+
+.last:hover::after {
+  width: 15%;
+}
+}
+
+
 </style>
