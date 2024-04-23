9bfe8c652f30a4da6c59ce085f1a9ef4ed476611
Mateusz <ogonekmateusz69@gmail.com>
2024-04-23 15:09:40 +0200

    add responsive service section

---
+++ src/components/servicesBlock.vue
@@ -0,0 +1,66 @@
+<template>
+    <!-- Pierwszy blok -->
+    <div class="col-lg-4">
+      <div class="card">
+        <div class="card-body">
+          <img :src="require(`../assets/${props.imgSrc}`)" alt="img">
+          <h5 class="card-title">{{ props.title }}</h5>
+          <p class="card-text">{{ props.description }}.</p>
+          <div class="greenLine"></div> <!-- Zachowana zielona linia -->
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+    import { defineProps } from 'vue';
+  
+    const props = defineProps<{
+      title: string,
+      imgSrc: string,
+      description: string
+    }>();
+  </script>
+  
+  <style scoped>
+  .card {
+    margin-bottom: 10px; /* zmniejszono odstęp górny i dolny */
+    background-color: var(--lightDark);
+    border-radius: 0 !important;
+    padding: 2rem 1.5625rem; /* zmniejszono padding */
+    position: relative; /* dodano pozycjonowanie */
+  }
+  
+  .card-title {
+    font-size: 1.4rem; /* zmniejszono czcionkę */
+    line-height: 1.6rem; /* zmniejszono odstęp między wierszami */
+    font-weight: bold;
+    color: var(--white);
+    margin: 0.8rem 0; /* zmniejszono marginesy */
+  }
+  
+  .card-text {
+    font-size: 0.9rem; /* zmniejszono czcionkę */
+    line-height: auto;
+    color: var(--gray);
+  }
+  
+  .greenLine {
+    background-color: var(--primary) !important;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 6px;
+    left: 0;
+  }
+  
+  @media(max-width:540px) {
+    .card-title {
+      font-size: 1.2rem;
+    }
+    .card-text {
+      font-size: 0.8rem; /* dostosowano czcionkę dla iPhone SE */
+    }
+  }
+  </style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,74 +1,62 @@
 <template>
- 
-    <SectionHero />
-    
-    <section class="services">
-      <div class="services-header">
-        <h2>7+ years experience working</h2>
-        <div class="services-header-right-block">
-          <div class="right-block-wrapper">
-            <hr class="line" />
-            Services
+  <SectionHero />
+  <!-- sekcja services -->
+  <section class="services ">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="services-header">
+            <h2>7+ years experience <br> working</h2>
           </div>
-          <p>Discover the best services I offer, designed to ensure the success of your project.</p>
+        </div>
+        <div class="col-lg-6">
+          <div class="services-header">
+            <div class="services-header-content">
+              <hr class="line"> <span>Services</span> 
+            </div>
+            <div class="services-description">
+              <p>Discover the best services I offer, designed to ensure the success of your project.</p>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="cardServices">
-        <SectionServices 
-          v-for="service in services" 
-          :key="service.title" 
-          :imgSrc="service.imgSrc" 
-          :title="service.title" 
-          :description="service.description" 
+    </div>
+    <!-- cards -->
+    <div class="row m-5">
+      <sevicesBlock
+          v-for="(service, index) in services"
+          :key="index"
+          :title="service.title"
+          :imgSrc="service.imgSrc"
+          :description="service.description"
         />
-      </div>
-    </section>
-    <section class="works">
-      <div class="top-block">
-        <hr class="line" />
-        <span>Recent Work</span>
-      </div>
-      
-      <div class="works-header">
-        <h2>Some of my favorite <br> projects.</h2>
-        <div class="services-header-right-block">
-          <button>View All Project</button>
-        </div>
-      </div>
-      
-      <div class="cardWorks">
-        <SectionWork 
-          v-for="work in works" 
-          :key="work.title" 
-          :imgSrc="work.imgSrc" 
-          :title="work.title" 
-          :href = "work.href"
-        />
-      </div>
-    </section>
-    <section class="contact">
-      <SectionContact/>
-    </section>
+    </div>
+    <!-- cards end -->
+  </section>
+  <!-- koniec sekcji services -->
 </template>
 
 <script setup lang="ts">
 import SectionHero from "../components/HomeViewComponents/SectionHero.vue";
+import sevicesBlock from "@/components/servicesBlock.vue";
+
 import SectionServices from "../components/HomeViewComponents/SectionServices.vue";
 import SectionWork from "@/components/HomeViewComponents/SectionWork.vue";
 import SectionContact from "@/components/HomeViewComponents/SectionContact.vue";
-interface service {
+
+interface Service {
   title: string;
   imgSrc: string;
   description: string;
 }
 
-interface work {
+interface Work {
   title: string;
   imgSrc: string;
   href: string;
 }
 
-const services: service[] = [
+const services: Service[] = [
   {
     title: "Product Designer",
     imgSrc: "icon-1.png",
@@ -86,7 +74,7 @@ const services: service[] = [
   }
 ];
 
-const works: work[] = [
+const works: Work[] = [
   {
     title: "Alana - live data app",
     imgSrc: "alana.png",
@@ -111,111 +99,49 @@ const works: work[] = [
 </script>
 
 <style>
-.wrapper {
-  width: 100%;
+.line {
+  background-color: var(--primary);
+  width: 50px;
+  border: 1.9px;
+  height: 2px;
+  float: left;
+  margin-right: 10px;
+  opacity: 1;
 }
 
-.services {
-  .cardServices {
-    width: 100%;
-    justify-content: center;
-    display: flex;
-  }
-
-  .services-header {
-    width: 100%;
-    justify-content: space-around;
-    display: flex;
-    align-items: center;
-
-    .services-header-right-block {
-      .right-block-wrapper {
-        .line {
-          background-color: var(--primary);
-          width: 50px;
-          border: 1.9px;
-          height: 2px;
-          float: left;
-          margin-right: 10px;
-        }
-        color: var(--primary);
-      }
-      p {
-        color: var(--gray);
-      }
-    }
-  }
+.services-header-content {
+  display: flex;
+  align-items: center;
 }
 
-h2 {
-  color: var(--white);
+.services-header-content span {
+  color: var(--primary);
   font-weight: bold;
-  font-size: 46px;
-  line-height: 56px;
+  font-size: 1.25rem;
 }
 
-.works {
-  margin-top: 50px;
-  background: var(--lightDark);
-  padding: 80px 100px;
-
-  .top-block {
-    width: 100%;
-    .line {
-      background-color: var(--primary);
-      width: 50px;
-      border: 1.9px;
-      height: 2px;
-      float: left;
-      margin-right: 10px;
-    }
-    span {
-      color: var(--primary);
-      font-weight: bold;
-    }
-  }
-
-  .works-header {
-    width: 100%;
-    justify-content: space-between;
-    display: flex;
-    align-items: center;
-    margin-bottom: 42px;
-
-    h2 {
-      margin: 10px 0;
-    }
-
-    .services-header-right-block {
-      button {
-        font-size: 14px;
-        background-color: transparent;
-        font-weight: bold;
-        padding: 10px 16px;
-        border: 1px solid var(--primary);
-        color: white;
-        cursor: pointer;
-        transition: color 0.3s ease-in-out;
-      }
-
-      button:hover {
-        background-color: var(--primary);
-        border-color: var(--lightDark);
-        color: var(--lightDark);
-      }
-    }
-  }
-  
-  .cardWorks {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    align-items: center;
-  }
+.services-header h2 {
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: var(--white);
 }
-.contact{
-    width: 100%;
-    height: 213px;
 
+.services-description {
+  font-size: 1.2rem;
+  color: var(--gray);
+}
+
+@media (max-width: 520px) {
+  .services-header h2 {
+    font-size: 1.5rem;
+  }
+
+  .services-description {
+    font-size: 1rem;
+  }
+
+  .services-header-content span {
+    font-size: 1.1rem;
+  }
 }
 </style>
